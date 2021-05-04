27e64eb43ba8b61727020b85aba9619057db070a
CarolineMano <carol.marinho.mano@gmail.com>
2021-05-04 19:11:13 -0300

    Update Main.java
    Troca da chamada dos métodos conforme novas alterações do Jogo.java

--- Main.java
+++ Main.java
@@ -2,10 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Jogo novoJogo = new Jogo();
 
-
-        novoJogo.imprimirDescricao();
-        novoJogo.escolherMaoComputador();
-        novoJogo.escolherMaoJogador();
-        novoJogo.decidirQuemComeca();
+        novoJogo.implementarJogo();
+        
     }
 }
