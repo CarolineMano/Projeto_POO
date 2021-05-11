68d92680bd79b07c8e6863bd6973bac5acdf6c53
CarolineMano <carol.marinho.mano@gmail.com>
2021-05-11 12:34:29 -0300

    Update Ferramenta.java
    Implementação de método para testar se o input é de fato número inteiro

--- Ferramenta.java
+++ Ferramenta.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ferramenta {
     public static void sleep (int milisegundos) {
 
@@ -15,5 +17,23 @@ public class Ferramenta {
         Ferramenta.sleep(2000);
         System.out.print(". ");
         System.out.println();
+    }
+
+    public static int testarInteiro(String mensagem, Scanner entrada) {
+        boolean testeInteiro = true;
+        String testeEscolha;
+        int escolha = 0;
+        do {
+            System.out.print(mensagem);
+            testeEscolha = entrada.next();
+            try {
+                escolha = Integer.parseInt(testeEscolha);
+                testeInteiro = false;
+            } catch (Exception e) {
+                System.out.println("Você não digitou um número!");
+                testeInteiro = true;
+            }
+        } while (testeInteiro);
+        return escolha;
     }
 }
